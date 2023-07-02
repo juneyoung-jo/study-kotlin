3b5e6ef585114f9cfbf2211df054be81a5f68c3b
juneyoung-jo@github.com <juneyoung-jo@github.com>
2023-07-02 23:24:54 +0900

    feat : chapter2 - 2

--- src/main/kotlin/Chapter2.kt
+++ src/main/kotlin/Chapter2.kt
@@ -10,9 +10,8 @@ fun main() {
     val la = ls + 42 // List<Any>로 추론
 
     val lla: MutableList<Any> = mutableListOf()
-    addAll(lla, ls)
-
-    // 2.16.2 공변성을 써야 하는 경우와 반공변성을 써야 하는 경우
+    addAll1(lla, ls)
+    addAll2(lla, ls)
 
 }
 
@@ -20,9 +19,17 @@ interface Bag<T> {
     fun get(): T
 }
 
-fun <T> addAll(
+fun <T> addAll1(
     list1: MutableList<T>,
     list2: MutableList<out T> // T를 공변성을 만듦
 ) {
     for (elem in list2) list1.add(elem)
+}
+
+fun <T> addAll2(
+    list1: MutableList<in T>,
+    list2: MutableList<T>
+) {
+    for (elem in list2)
+        list1.add(elem)
 }
