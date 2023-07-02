f25597d6cdd7a87ac4606415ea9d25b7d0d7dec4
juneyoung-jo@github.com <juneyoung-jo@github.com>
2023-07-03 01:52:37 +0900

    feat : chapter3 (2)

--- src/main/kotlin/Chapter3.kt
+++ src/main/kotlin/Chapter3.kt
@@ -10,6 +10,11 @@ fun main() {
     // 3.3.3 고차 함수 구현하기
     println("squareOfTriple2: ${squareOfTriple2(2)}") // 36
 
+    // 3.3.5 익명 함수 사용하기
+    val cos = higherCompose<Double, Double, Double>()({ x: Double -> Math.PI / 2 - x })(Math::sin)
+    println("cos(2.0): ${cos(2.0)}")
+
+
 }
 
 
@@ -36,5 +41,58 @@ val add: (Int) -> (Int) -> Int = { a -> { b -> a + b } }
 val square: (Int) -> Int = { x -> x * x }
 val triple: (Int) -> Int = { x -> x * 3 }
 val compose3: ((Int) -> Int) -> ((Int) -> Int) -> (Int) -> Int =
-    { x -> { y -> { z -> x(y(z)) } } }
+    { x ->
+        { y ->
+            { z ->
+                x(y(z))
+            }
+        }
+    }
 val squareOfTriple2 = compose3(square)(triple)
+
+// 연습문제 3-5
+fun <T, U, V> higherCompose(): ((U) -> V) -> ((T) -> U) -> (T) -> V =
+    { f ->
+        { g ->
+            { x ->
+                f(g(x))
+            }
+        }
+    }
+
+val squareOfTriple3 = higherCompose<Int, Int, Int>()(square)(triple)
+
+// 연습문제 3-6
+fun <T, U, V> higherAndThen(): ((T) -> U) -> ((U) -> V) -> (T) -> V =
+    { f ->
+        { g ->
+            { x ->
+                g(f(x))
+            }
+        }
+    }
+
+// 3.3.6 로컬 함수 정의하기
+fun cos(arg: Double): Double {
+    fun f(x: Double): Double = Math.PI / 2 - x
+    fun sin(x: Double): Double = Math.sin(x)
+    return compose2(::f, ::sin)(arg)
+}
+
+// 연습문제 3-7
+fun <A, B, C> partialA(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
+
+// 연습문제 3-8
+fun <A, B, C> partialB(b: B, f: (A) -> (B) -> C): (A) -> C = { a: A -> f(a)(b) }
+
+// 연습문제 3-9
+fun <A, B, C, D> curried(): (A) -> (B) -> (C) -> (D) -> String = { a ->
+    { b ->
+        { c ->
+            { d ->
+                "$a, $b, $c, $d"
+            }
+        }
+    }
+}
+
